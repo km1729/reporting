795c467a775d9d2369e099579f26f3d4ea83ad5d
Kelly McIntyre <kellymcintyre@outlook.com.au>
2021-11-26 17:24:01 +1100

    updated read and save usage data - added split, strip, date to column

--- code/combine_reports.py
+++ code/combine_reports.py
@@ -4,6 +4,7 @@
 
 import os
 from pathlib import Path
+import csv
 
 report_dir = './report/temp'
 p = Path(report_dir)
@@ -19,8 +20,10 @@ def get_date(fname):
     return year, month, date
 
 def read_a_report(report):
+    
     f = open(report_dir+'/'+report.name,'r') 
-    return f.readlines()
+    a = f.readlines()
+    return a
 
 
 for report in os.scandir(p):
@@ -30,17 +33,25 @@ for report in os.scandir(p):
         # determine a report name has a target report name
         # get the date of the report
         if 'dp9_gdata_20' in report.name: 
-            yy, mm, dd = get_date(report.name) 
             data_in_report = read_a_report(report)
 
             # read the project name and determine if there is a project name is 'dp9' in a line of the report
             # dp_2021xxxx.rpt file doesn't have a project name in a daily report - different format
-            for proj_usage in data_in_report:              
-                if 'dp' in proj_usage:
-                    a = proj_usage.split()    
-                    print(a)                
+            for proj_usage in data_in_report:
+                yy, mm, dd = get_date(report.name) 
+                              
+                if 'dp' in proj_usage:                    
+                    a = proj_usage.split(' ')
+                    b = [elem.strip() for elem in a]
+                    b = list(filter(None,b))                    
+                    
+                    # add date
+                    b.insert(0,yy+mm+dd)
+                    print(b)
+                    # print(type(a))
+                    # write
                     f= open(combined_report1_path,'a')
-                    f.writelines(a)
+                    for word in b:              
+                        f.write(word + ',')                        
+                    f.write('\n')
                     f.close()          
-        
-
