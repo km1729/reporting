b16a5fd0a63a147fd3582b172f8cf09655eb4c4e
Kelly <kellymcintyre@outlook.com.au>
2021-12-07 21:25:23 +1100

    add a constraint of list length when it copies the target project record

--- code/combine_reports.py
+++ code/combine_reports.py
@@ -6,9 +6,9 @@ import os
 from pathlib import Path
 import csv
 
-report_dir = './report/temp'
-p = Path(report_dir)
-combined_report1_path = './report/total_dp9_gdata.csv'
+origin_path = './report'
+input_dir = Path(origin_path)
+dest_path = './report/dataset_output/total_dp9_gdata.csv'
 
 # target_reports = ['dp9_20','dp9_gdata_20','dp9_scratch_20']
 # project = ['dp9','dx2']
@@ -21,7 +21,7 @@ def get_date(fname):
 
 def read_a_report(report):
     
-    f = open(report_dir+'/'+report.name,'r') 
+    f = open(origin_path+'/'+report.name,'r') 
     a = f.readlines()
     return a
 
@@ -34,7 +34,7 @@ def convert_data_value_TB(usage):
         value /= 1000.0
     if unit == "MB":
         value /= 1000000.0
-    if unit == "KB":
+    if unit == "kB":  
         value /= 1000000000.0
     return str(value)
 
@@ -47,40 +47,47 @@ def convert_data_value_GB(usage):
         value *= 0.001
     if unit == "MB":
         value /= 1000.0
-    if unit == "KB":
+    if unit == "kB":
         value /= 1000000.0
     return str(value)
 
-for report in os.scandir(p):
+count_dp9 = 0
+number_of_gdata = 0  
 
-    if report.is_file():
+for report in os.scandir(input_dir):
+   
+    number_of_files = len(os.listdir(input_dir))
 
+    if report.is_file():                
+        
         # determine a report name has a target report name
-        # get the date of the report
-        if 'dp9_gdata_20' in report.name: 
-            data_in_report = read_a_report(report)
+        if 'dp9_gdata_20' in report.name:
+            number_of_gdata +=1 
+            proj_data = read_a_report(report)
 
-            # read the project name and determine if there is a project name is 'dp9' in a line of the report
-            # dp_2021xxxx.rpt file doesn't have a project name in a daily report - different format
-            for proj_usage in data_in_report:
+            # read the daily report and determine if there is a project name  'dp9' in a line of the report
+            for proj in proj_data:
                 yy, mm, dd = get_date(report.name) 
-                              
-                if 'dp' in proj_usage:                    
-                    a = proj_usage.split(' ')
+
+                if 'dp' in proj:   
+                    a = proj.split(' ')                    
                     b = [elem.strip() for elem in a]
                     b = list(filter(None,b))                    
+                                        
+                    b.insert(0,str(yy+'-'+mm+'-'+dd))              
                     
-                    # add date
-                    b.insert(0,str(yy+'-'+mm+'-'+dd))
-                    print(b[3])
-                    b.insert(4,convert_data_value_GB(b[3]))
-                    b.insert(5,convert_data_value_TB(b[3]))
-                    print(b)
-                    
+                    if len(b) == 6:
+                        count_dp9 +=1 
+                        b.insert(4,convert_data_value_GB(b[3]))                    
+                        b.insert(5,convert_data_value_TB(b[3]))    
+                        
+                        f= open(dest_path,'a')
+                        for word in b:              
+                            f.write(word + ',')                        
+                        f.write('\n')
+                        f.close()                    
+                              
 
-                    # write to combinded dataset file
-                    f= open(combined_report1_path,'a')
-                    for word in b:              
-                        f.write(word + ',')                        
-                    f.write('\n')
-                    f.close()          
+print(str(number_of_files)+" of files found in this directory")
+print(str(number_of_gdata) + " of dp9_gdata file found")
+print(str(count_dp9) + " of records copied ")
