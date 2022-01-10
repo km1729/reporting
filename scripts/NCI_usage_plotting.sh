76567d75767b58581bc9d1d40fa701e9d7d9f8d5
Kelly <kellymcintyre@outlook.com.au>
2022-01-10 16:25:33 +1100

    delete folders

--- scripts/NCI_usage_plotting.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-module use ~access/modules
-module load bom_envs/python_3.7.5
-
-
-python /g/data/dp9/admin/script/nci_usage_plotting.py --config /g/data/dp9/admin/script/nci_usage_config.ini
